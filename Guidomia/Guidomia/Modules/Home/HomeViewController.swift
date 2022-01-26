def7967ae6dbdeccc707f39479b36a56ad1754f0
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-01-27 04:06:57 +0800

    Rename ViewController to HomeViewController

--- Guidomia/Guidomia/Modules/Home/HomeViewController.swift
+++ Guidomia/Guidomia/Modules/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Guidomia
 //
 //  Created by Failyn Kaye Sedik on 1/27/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
