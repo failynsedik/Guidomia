984ed99d6f23c363e274706b850ceaaaf03b2428
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-01-27 04:23:13 +0800

    Add a custom palette for the app

---
+++ Guidomia/Guidomia/Utilities/UIColorExtensions/UIColor+Palette.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Palette.swift
+//  Guidomia
+//
+//  Created by Failyn Kaye Sedik on 1/27/22.
+//
+
+import UIKit
+
+extension UIColor {
+    static let gOrange = UIColor(rgb: 0xFC6016)
+    static let gDarkGray = UIColor(rgb: 0x858585)
+    static let gLightGray = UIColor(rgb: 0xD5D5D5)
+}
