29b3c7340eb98803afd79eaa08750cdf3dc77b96
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-01-27 04:24:00 +0800

    Add initial config of the navigation bar

--- Guidomia/Guidomia/Modules/Home/HomeViewController.swift
+++ Guidomia/Guidomia/Modules/Home/HomeViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupNav()
+        setupViews()
+    }
+}
+
+// MARK: - UI Initial Setup
+
+extension HomeViewController {
+    private func setupNav() {
+        let leftBarButtonItem = UIBarButtonItem(title: "Guidomia", style: .plain, target: nil, action: nil)
+        leftBarButtonItem.tintColor = .white
+        navigationItem.leftBarButtonItem = leftBarButtonItem
+
+        let rightBarButtonItem = UIBarButtonItem(title: "⠇", style: .plain, target: nil, action: nil)
+        rightBarButtonItem.tintColor = .white
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .gOrange
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .white
     }
 }
