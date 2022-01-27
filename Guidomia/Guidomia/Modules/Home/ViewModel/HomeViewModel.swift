cba93c9013c399ac3bf4f1f86d7ea33053ed9dbf
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-01-27 09:57:27 +0800

    Add tableview data source

--- Guidomia/Guidomia/Modules/Home/ViewModel/HomeViewModel.swift
+++ Guidomia/Guidomia/Modules/Home/ViewModel/HomeViewModel.swift
@@ -16,3 +16,38 @@ struct HomeViewModel {
     }
 }
 
+// MARK: - TableView Data Source
+
+extension HomeViewModel {
+    func numberOfRows() -> Int {
+        cars.count
+    }
+
+    /// Returns a "ready" (meaning, already formatted) model for `CarDetailCollapsedCellContent`.
+    func getCarDetailCollapsedCellContent(at row: Int) -> CarDetailCollapsedCellContent? {
+        guard let car = cars[safe: row] else { return nil }
+
+        let name = "\(car.make) \(car.model)"
+        let imageName = "\(name.split(separator: " ").joined(separator: "_")).jpg"
+        let price = "Price : \(car.marketPrice.roundedWithAbbreviations)"
+
+        var starRatings: [String] = []
+
+        for _ in 0 ..< car.rating {
+            starRatings.append("★")
+        }
+
+        let rating = starRatings.joined(separator: " ")
+        let isLastCell = row == cars.count - 1
+
+        let content = CarDetailCollapsedCellContent(
+            name: name,
+            imageName: imageName,
+            price: price,
+            rating: rating,
+            isLastCell: isLastCell
+        )
+
+        return content
+    }
+}
