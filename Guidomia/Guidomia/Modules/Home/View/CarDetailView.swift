a9a0145802c5d1d3202d326f4f9ae27be93ec007
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-01-27 12:03:42 +0800

    Implement UI for expanded cell on CarDetailView

--- Guidomia/Guidomia/Modules/Home/View/CarDetailView.swift
+++ Guidomia/Guidomia/Modules/Home/View/CarDetailView.swift
@@ -41,6 +41,36 @@ final class CarDetailView: UIView {
         return label
     }()
 
+    private let prosTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Pros :"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        label.textColor = UIColor(rgb: 0x000000, alpha: 0.45)
+        return label
+    }()
+
+    private let consTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Cons :"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        label.textColor = UIColor(rgb: 0x000000, alpha: 0.45)
+        return label
+    }()
+
+    private let prosStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        return stackView
+    }()
+
+    private let consStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        return stackView
+    }()
+
     // MARK: - Lifecycle
 
     override init(frame: CGRect) {
@@ -60,6 +90,9 @@ extension CarDetailView {
     private func setupViews() {
         backgroundColor = .gLightGray
 
+        // The following are the common subviews and constraints
+        // between the collapsed and expanded states
+
         addSubview(imageView)
         addSubview(nameLabel)
         addSubview(priceLabel)
@@ -69,7 +102,6 @@ extension CarDetailView {
             $0.width.equalTo(126)
             $0.height.equalTo(64)
             $0.top.leading.equalToSuperview().inset(16)
-            $0.bottom.equalToSuperview().inset(16).priority(.medium)
         }
 
         nameLabel.snp.makeConstraints {
@@ -81,12 +113,6 @@ extension CarDetailView {
             $0.top.equalTo(nameLabel.snp.bottom)
             $0.leading.trailing.equalTo(nameLabel)
         }
-
-        ratingLabel.snp.makeConstraints {
-            $0.top.equalTo(priceLabel.snp.bottom).offset(8)
-            $0.leading.trailing.equalTo(nameLabel)
-            $0.bottom.equalToSuperview().inset(16).priority(.high)
-        }
     }
 }
 
@@ -94,9 +120,89 @@ extension CarDetailView {
 
 extension CarDetailView {
     func setup(collapsedContent: CarDetailCollapsedCellContent) {
+        // Set content
         imageView.image = UIImage(named: collapsedContent.imageName)
         nameLabel.text = collapsedContent.name
         priceLabel.text = collapsedContent.price
         ratingLabel.text = collapsedContent.rating
+
+        // Remove subviews for expanded cell
+        prosTitleLabel.removeFromSuperview()
+        prosStackView.removeAllArrangedSubviews()
+        prosStackView.removeFromSuperview()
+        consTitleLabel.removeFromSuperview()
+        consStackView.removeAllArrangedSubviews()
+        consStackView.removeFromSuperview()
+
+        // Setup constraints
+        ratingLabel.snp.remakeConstraints {
+            $0.top.equalTo(priceLabel.snp.bottom).offset(8)
+            $0.leading.trailing.equalTo(nameLabel)
+            $0.bottom.equalToSuperview().inset(16)
+        }
+    }
+
+    func setup(expandedContent: CarDetailExpandedCellContent) {
+        // Set content
+        imageView.image = UIImage(named: expandedContent.collapsedContent.imageName)
+        nameLabel.text = expandedContent.collapsedContent.name
+        priceLabel.text = expandedContent.collapsedContent.price
+        ratingLabel.text = expandedContent.collapsedContent.rating
+
+        // Reset stackview's configurations
+        prosStackView.removeAllArrangedSubviews()
+        prosStackView.removeFromSuperview()
+        consStackView.removeAllArrangedSubviews()
+        consStackView.removeFromSuperview()
+
+        // Add arranged subviews for the stackviews
+        for pros in expandedContent.pros {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+            label.attributedText = pros
+            label.numberOfLines = 0
+            prosStackView.addArrangedSubview(label)
+        }
+
+        for cons in expandedContent.cons {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+            label.attributedText = cons
+            label.numberOfLines = 0
+            consStackView.addArrangedSubview(label)
+        }
+
+        // Add subviews for expanded cell
+        addSubview(prosTitleLabel)
+        addSubview(prosStackView)
+        addSubview(consTitleLabel)
+        addSubview(consStackView)
+
+        // Setup constraints
+        ratingLabel.snp.remakeConstraints {
+            $0.top.equalTo(priceLabel.snp.bottom).offset(8)
+            $0.leading.trailing.equalTo(nameLabel)
+        }
+
+        prosTitleLabel.snp.remakeConstraints {
+            $0.top.equalTo(ratingLabel.snp.bottom).offset(24)
+            $0.leading.trailing.equalToSuperview().inset(32)
+        }
+
+        prosStackView.snp.remakeConstraints {
+            $0.top.equalTo(prosTitleLabel.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview().inset(32)
+        }
+
+        consTitleLabel.snp.remakeConstraints {
+            $0.top.equalTo(prosStackView.snp.bottom).offset(14)
+            $0.leading.trailing.equalToSuperview().inset(32)
+        }
+
+        consStackView.snp.remakeConstraints {
+            $0.top.equalTo(consTitleLabel.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview().inset(32)
+            $0.bottom.equalToSuperview().inset(32)
+        }
     }
 }
