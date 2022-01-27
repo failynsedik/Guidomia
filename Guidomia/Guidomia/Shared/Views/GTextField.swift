73cb756d5d85fff5e9b8bb0767a78030b87fd1c3
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-01-28 05:22:31 +0800

    Add custom UITextField to handle textfield padding

---
+++ Guidomia/Guidomia/Shared/Views/GTextField.swift
@@ -0,0 +1,21 @@
+//
+//  GTextField.swift
+//  Guidomia
+//
+//  Created by Failyn Kaye Sedik on 1/28/22.
+//
+
+import UIKit
+
+class GTextField: UITextField {
+    var leftPadding: CGFloat = 0
+    var rightPadding: CGFloat = 0
+
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        CGRect(x: bounds.origin.x + leftPadding, y: bounds.origin.y, width: bounds.width - rightPadding * 2, height: bounds.height)
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        CGRect(x: bounds.origin.x + leftPadding, y: bounds.origin.y, width: bounds.width - rightPadding * 2, height: bounds.height)
+    }
+}
