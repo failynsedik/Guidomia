6ea91cea630695794e42d7c10d6ed8743cbf2f5b
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-01-27 09:58:13 +0800

    Implement collapsed UI with data

--- Guidomia/Guidomia/Modules/Home/HomeViewController.swift
+++ Guidomia/Guidomia/Modules/Home/HomeViewController.swift
@@ -7,7 +7,26 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
+    private let viewModel = HomeViewModel()
+
+    // MARK: - Subview
+
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .white
+        tableView.showsVerticalScrollIndicator = false
+        tableView.separatorStyle = .none
+        let clearView = UIView()
+        clearView.backgroundColor = .clear
+        tableView.tableHeaderView = clearView
+        tableView.tableFooterView = clearView
+        tableView.register(CarDetailTableViewCell.self, forCellReuseIdentifier: CarDetailTableViewCell.reuseIdentifier)
+        return tableView
+    }()
+
+    // MARK: - View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,5 +56,44 @@ extension HomeViewController {
 
     private func setupViews() {
         view.backgroundColor = .white
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        view.addSubview(tableView)
+
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_: UITableView, didSelectRowAt _: IndexPath) {
+        // Do nothing for now...
+    }
+
+    func tableView(_: UITableView, heightForRowAt _: IndexPath) -> CGFloat {
+        UITableView.automaticDimension
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        viewModel.numberOfRows()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: CarDetailTableViewCell.reuseIdentifier, for: indexPath) as? CarDetailTableViewCell,
+           let content = viewModel.getCarDetailCollapsedCellContent(at: indexPath.row)
+        {
+            cell.setup(collapsedContent: content)
+            return cell
+        }
+
+        return UITableViewCell()
     }
 }
