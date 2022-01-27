c5922da24a08afdb3839c4e77088f68e99e914bf
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-01-27 09:52:40 +0800

    Add models for collapsed and expanded cell

---
+++ Guidomia/Guidomia/Modules/Home/Model/CarDetailCollapsedCellContent.swift
@@ -0,0 +1,16 @@
+//
+//  CarDetailCellContent.swift
+//  Guidomia
+//
+//  Created by Failyn Kaye Sedik on 1/27/22.
+//
+
+import Foundation
+
+struct CarDetailCollapsedCellContent {
+    let name: String
+    let imageName: String
+    let price: String
+    let rating: String
+    let isLastCell: Bool
+}
---
+++ Guidomia/Guidomia/Modules/Home/Model/CarDetailExpandedCellContent.swift
@@ -0,0 +1,14 @@
+//
+//  CarDetailExpandedCellContent.swift
+//  Guidomia
+//
+//  Created by Failyn Kaye Sedik on 1/27/22.
+//
+
+import Foundation
+
+struct CarDetailExpandedCellContent {
+    let collapsedContent: CarDetailCollapsedCellContent
+    let pros: [String]
+    let cons: [String]
+}
